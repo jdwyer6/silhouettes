f76330b05fc67005cf48e017142360be415997e8
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-07-21 09:41:49 -0500

    work on filtering thumbnails

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -4,17 +4,28 @@ import defaultImage from '../assets/images/sunset/default-image.png';
 import blankCanvas from '../assets/images/background/blankCanvas.jpg';
 // import OptionCard from '../options/OptionCard';
 // import { selectGender } from '../options/optionSlice';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 const CreatePage = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [selectedId, setId] = useState(0);
+    // const [gender, setGender] = useState("");
+    // const [age, setAge] = useState("");
+    const [dog, setDog] = useState("");
+    // const [bodyLanguage, setBodyLanguage] = useState("");
+    // console.log(gender + age + dog + bodyLanguage)
 
-    const handleClick = (clickedId) => {
-        setId(clickedId)
-        console.log(selectedId);
-    }
+    // const genderRef = useRef();
+    // function updateGender(e){const gender = genderRef.current.value}
+
+    const [human, setHuman] = useState({
+        gender: '',
+        age: '',
+        bodyLanguage: ''
+    })
+    console.log(`Gender: ${human.gender} - Age: ${human.age} - Body Language: ${human.bodyLanguage}`);
+
+
+
 
     return ( 
         <Container>
@@ -30,16 +41,45 @@ const CreatePage = () => {
                         <Button>Customize</Button>
                     </div>
                     <form>
-                        <div className="my-3 gender-container">
-                            <label for='gender'>Gender:</label>
-                            <select onChange={event => {setSearchTerm(event.target.value)}} name="gender" id="gender" className="form-select" aria-label="gender">
+                        <div className="my-3">
+                            <label htmlFor='gender'>Gender:</label>
+                            <select onChange={(e) => {setHuman({gender: e.target.value})}} name="gender" id="gender" className="form-select" aria-label="gender" defaultValue={'male'}>
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                                 <option value="all-genders">All</option>
                             </select>
                         </div>
+                        <div className="my-3">
+                            <label htmlFor='age'>Age:</label>
+                            <select onChange={(e) => {setHuman({age: e.target.value})}} name="age" id="age" className="form-select" aria-label="age" defaultValue={"adult"}>
+                                <option value="child">Child</option>
+                                <option value="teen">Teen</option>
+                                <option value="adult">Adult</option>
+                                <option value="elderly">Elderly</option>
+                            </select>
+                        </div>
+                        <div className="my-3">
+                            <label htmlFor='body-language'>Body Language:</label>
+                            <select onChange={(e) => {setHuman({bodyLanguage: e.target.value})}} name="body-language" id="body-language" className="form-select" aria-label="body-language" defaultValue={"adult"}>
+                                <option value="Standing">Standing</option>
+                                <option value="Seated">Seated</option>
+                                <option value="Running">Running</option>
+                            </select>
+                        </div>
                         <Row>
-                            {HUMANIMAGELAYERS.filter((val) => {
+                            {/* {HUMANIMAGELAYERS.filter((val) => {
+                                if(gender === 'male')
+                                    return val
+                            })} */}
+                            {HUMANIMAGELAYERS.map((img) => {
+                                return(
+                                    <div className="option-container">
+                                        <img className='option-container__image' src={img.image}></img>
+                                    </div>
+                                )
+                            })}
+
+                            {/* {HUMANIMAGELAYERS.filter((val) => {
                                 if(searchTerm == ''){
                                     return val
                                 }else if(searchTerm == 'all-genders'){
@@ -53,15 +93,15 @@ const CreatePage = () => {
                                         <img className='option-container__image' src={val.image}></img>
                                     </div>
                                 ) 
-                            })}
+                            })} */}
                         </Row>
 
 
                         
 
-                        <div className="my-3 breed-container">
-                            <label for='breed'>Dog Breed:</label>
-                            <select name="breed" id="breed" class="form-select" aria-label="Dog breed">
+                        <div className="my-3 dog-container">
+                            <label htmlFor='dog'>Dog Breed:</label>
+                            <select onChange={(e) => {setDog(e.target.value)}} name="dog" id="dog" className="form-select" aria-label="Dog dog">
                                 <option value="German Shepard">German Shepard</option>
                                 <option value="Australian Shepard">Australian Shepard</option>
                                 <option value="Chihuahua">Chihuahua</option>
